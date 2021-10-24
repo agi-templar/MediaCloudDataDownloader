ed2ee80d6546cd59fd7408bc78cb5f67f2710eca
ruiboliu <Ruibo.Liu.GR@dartmouth.edu>
2021-10-24 10:25:37 -0400

    small edits

--- run_download.py
+++ run_download.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 # coding=utf-8
 
-# Author: Ruibo Liu (ruibo.liu.gr@dartmoputh.edu)
+# Author: Ruibo Liu (ruibo.liu.gr@dartmouth.edu)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
